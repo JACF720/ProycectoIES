f11e4abaa5fa5cd284d3d863eb50d1f21bb8bc7a
Juan Antonio Torres <jat_antonio_torres.juan@outlook.com>
2022-01-08 16:51:40 -0600

    se agrego test

---
+++ src/test/TestUsuario.java
@@ -0,0 +1,10 @@
+package test;
+
+import domain.Persona;
+
+public class TestUsuario {
+    public static void main(String[] args) {
+        Persona persona1 = new Persona(1,"Juan","Antonio","Torres","5jaun5","juan@gmail.com","juan1234");
+        System.out.println("persona1 = " + persona1);
+    }
+}
